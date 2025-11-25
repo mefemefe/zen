e23b54f42cbcd60a035e45e042d06ac3ddd22da2
mefe <pablogrosana@gmail.com>
2025-11-25 05:01:05 -0300

    first version
    first version
    
    update sw
    
    icon

---
+++ script.js
@@ -0,0 +1,32 @@
+// Simple text synchronizer
+const label = document.getElementById('label');
+
+function syncText() {
+    // Reset
+    label.innerText = "Inhale";
+    label.style.opacity = 1;
+
+    // Hold at 4s
+    setTimeout(() => {
+        label.innerText = "Hold";
+    }, 4000);
+
+    // Exhale at 11s (4+7)
+    setTimeout(() => {
+        label.innerText = "Exhale";
+    }, 11000);
+
+    // Fade out text slightly at the end of exhale
+    setTimeout(() => {
+        label.style.opacity = 0.5;
+    }, 18000);
+}
+
+syncText();
+setInterval(syncText, 19000);
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then(() => {
+    console.log('Service Worker Registered');
+  });
+}
---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'zen-v1';
+const ASSETS = ['/'];
+
+// Install event: cache essential files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate event: clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch event: serve requests from cache if offline, else network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
